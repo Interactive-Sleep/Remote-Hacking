d88464a71147e7cda82e36978e44ebcb7b2d89df
Antony <86053464+AntonyLoose@users.noreply.github.com>
2023-03-16 13:18:53 +1100

    Imported code for printing IR hex codes

--- RemoteDecoding/RemoteDecoding.ino
+++ RemoteDecoding/RemoteDecoding.ino
@@ -1,12 +1,26 @@
 // Tutorial here: https://www.instructables.com/IR-Remote-Hacking-and-Automation/
 
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
+void setup()
+{
+  // Make sure the terminal you have open is running on 9600, otherwise you will not be able to see the codes
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if (irrecv.decode(&results))
+    {
+     Serial.println(results.value, HEX);
+     irrecv.resume(); // Receive the next value
+    }
 }
